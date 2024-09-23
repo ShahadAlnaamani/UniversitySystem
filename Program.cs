bc31b5378dd092ac1953b35a810abf622d658066
Shahad <Shahadalnaamani@outlook.com>
2024-09-23 12:26:02 +0400

    Added options to admin page

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace UniversityCourses
     {
         //CourseID || Student Names 
         Dictionary <string, HashSet <string>> Courses = new Dictionary <string, HashSet<string>> ();
-        HashSet<string> StudentNames = new HashSet<string> ();
+        //HashSet<string> StudentNames = new HashSet<string> ();
 
         static void Main(string[] args)
         {
@@ -15,7 +15,7 @@ namespace UniversityCourses
             do
             {
                 Console.Clear();
-                Console.WriteLine("C I T Y   U N I V E R S I T Y");
+                Console.WriteLine("\n\nC I T Y   U N I V E R S I T Y\n\n");
                 Console.WriteLine("\n\nChoose one of the following options:");
                 Console.WriteLine("1.  Admin");
                 Console.WriteLine("2.  Students ");
@@ -34,7 +34,7 @@ namespace UniversityCourses
                     case 1:
                         Admin();
                         break;
-
+                        
                     case 2:
                         Student();
                         break;
@@ -60,35 +60,103 @@ namespace UniversityCourses
 
         }
 
+
+        //*********************************ADMIN*******************************//
         static public void Admin()
         {
             //Go to admin --> view full course information[all courses and number of students enrolled and students in each course], add new course, remove course[check no one is enrolled], remove student from course, add student to course, search for student by course, search by student
-            Console.WriteLine("Hi from admin");
-            AdminLogin();
+            //View drop requests 
+            bool AdminPage = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\n\nC I T Y   U N I V E R S I T Y\n\n");
+                AdminLogin();
+
+                
+                Console.WriteLine("\n\nChoose one of the following options:");
+                Console.WriteLine("1.  View Course Information");
+                Console.WriteLine("2.  Add course");
+                Console.WriteLine("3.  Remove course");
+                Console.WriteLine("4.  Manage students"); //view student profile(courses), remove student profile, remove course for specific student, add student to course
+                Console.WriteLine("5.  Search for student by course");
+                Console.Write("\nEnter: ");
+
+                int Identity = 0;
+
+                try
+                {
+                    Identity = int.Parse(Console.ReadLine());
+                }
+                catch (Exception exception) { Console.WriteLine(exception.Message); }
+
+                switch (Identity) 
+                {
+                    case 1:
+                        ViewCourses();
+                        break;
+
+                    case 2:
+                        AddCourse();
+                        break;
+
+                    case 3:
+                        RemoveCourse();
+                        break;
+
+                    case 4:
+                        ManageStudent();
+                        break;
+
+                    case 5:
+                        SearchByCourse();
+                        break;
+
+                    default:
+                        Console.WriteLine("\n<!>Improper input please try again :( <!>");
+                        break;
+                }
+                
+            }while (AdminPage != false);
         }
-
-
-        static public void Student()
-        {
-            //Go to User --> view courses, enroll in course, remove course enrollment[check wait list], drop out [check waitlist, remove from all enrolled courses], view profile
-            Console.WriteLine("Hi from Student");
-            StudentLogin();
-        }
-
 
         static public void AdminLogin()
         {
+            Console.Clear();
+            Console.WriteLine("\n\nC I T Y   U N I V E R S I T Y\n\n");
             Console.WriteLine("Hi from Admin Login");
         }
 
 
+
+
+
+
+//****************************STUDENT***********************************************************//
+
+        static public void Student()
+        {
+            //Go to User --> view courses, enroll in course, remove course enrollment[check wait list], drop out [check waitlist, remove from all enrolled courses], view profile
+            //Request tp drop 
+            Console.Clear();
+            Console.WriteLine("\n\nC I T Y   U N I V E R S I T Y\n\n");
+            Console.WriteLine("Hi from Student");
+            StudentLogin();
+        }
+
         static public void StudentLogin()
         {
+            Console.Clear();
+            Console.WriteLine("\n\nC I T Y   U N I V E R S I T Y\n\n");
             Console.WriteLine("Hi from Student Login");
         }
 
 
         static public void WaitList()
-        { }
+        {
+            Console.Clear();
+            Console.WriteLine("\n\nC I T Y   U N I V E R S I T Y\n\n");
+        }
     }
 }
