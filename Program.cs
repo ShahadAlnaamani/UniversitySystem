4397e986377b1efca91c691d644a677b56e9f48f
Shahad <Shahadalnaamani@outlook.com>
2024-09-23 09:54:01 +0400

    connecting to repo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UniversityCourses
+{
+    internal class Program
+    {
+        //CourseID || Student Names 
+        Dictionary <int, string> Courses = new Dictionary <int, string> ();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C I T Y   L I B R A R Y");
+
+
+            //Go to admin --> view full course information 
+
+
+            //Go to User --> view courses, sign into course, remove course[check wait list], drop out [check waitlist]
+            
+        }
+
+
+        static public void WaitList()
+        { }
+    }
+}
