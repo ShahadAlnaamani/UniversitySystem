1a15f1a80cfbd027105aac1517aa744aee84c240
Shahad <Shahadalnaamani@outlook.com>
2024-09-23 11:05:52 +0400

    Added plan of program structure

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@
             Console.WriteLine("C I T Y   L I B R A R Y");
 
 
-            //Go to admin --> view full course information 
+            //Go to admin --> view full course information[all courses and number of students enrolled and students in each course], add new course, remove course[check no one is enrolled], remove student from course, add student to course, search for student by course, search by student
 
 
-            //Go to User --> view courses, sign into course, remove course[check wait list], drop out [check waitlist]
-            
+            //Go to User --> view courses, enroll in course, remove course enrollment[check wait list], drop out [check waitlist, remove from all enrolled courses], view profile 
+           
         }
 
 
